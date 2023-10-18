c276598ab36a5ac25e937ea6b112c96e779cbb18
ptphong131202 <ptphonggithub@gmail.com>
2023-10-18 19:40:10 +0700

    Upload project at 18/10/2023

---
+++ reactjs/src/services/userService.js
@@ -0,0 +1,39 @@
+import axios from '../axios'
+
+// function handle login
+const handleLoginApi = ( email, password ) =>
+{
+    return axios.post( 'api/login', { email, password } ); // request len server
+}
+
+const getAllUser = ( id ) =>
+{
+    return axios.get( `api/get-all-users?id=${ id }` ); // request len server
+}
+
+const createNewUserService = ( data ) =>
+{
+    return axios.post( `api/create-new-user`, data );
+}
+
+const deleteUserService = ( userid ) =>
+{
+    return axios.delete( `api/delete-user`,
+        {
+            data: { id: userid },
+        } );
+}
+
+const editUserService = ( inputData ) =>
+{
+    return axios.put( `api/edit-user`, inputData );
+}
+
+export
+{
+    handleLoginApi,
+    getAllUser,
+    createNewUserService,
+    deleteUserService,
+    editUserService
+};
